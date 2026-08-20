64a1010c241f94606acaac4f2bd58a8432b6f089
gc693ih <gabrielcardas62@gmail.com>
2026-08-20 09:01:29 +0200

    Project init: Spring Boot + Gradle + Mongo via Docker

---
+++ src/main/java/cardas/gabriel/orderproductapi/OrderProductApiApplication.java
@@ -0,0 +1,13 @@
+package cardas.gabriel.orderproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProductApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProductApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cardas/gabriel/orderproductapi/OrderProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cardas.gabriel.orderproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
